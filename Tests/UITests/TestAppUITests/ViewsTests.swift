696b8ebf7280e14720d55d5249e39516e2fdb801
Andreas Bauer <andi.bauer@tum.de>
2023-09-02 14:53:29 +0200

    Try to improve stability to Canvas UI tests

--- Tests/UITests/TestAppUITests/ViewsTests.swift
+++ Tests/UITests/TestAppUITests/ViewsTests.swift
@@ -39,6 +39,7 @@ final class ViewsTests: XCTestCase {
         XCTAssert(app.buttons["Show Tool Picker"].waitForExistence(timeout: 2))
         app.buttons["Show Tool Picker"].tap()
         
+        sleep(2) // waitForExistence will otherwise return immediately
         XCTAssertFalse(app.scrollViews.otherElements.images["palette_tool_pencil_base"].waitForExistence(timeout: 2))
         canvasView.swipeUp()
     }
