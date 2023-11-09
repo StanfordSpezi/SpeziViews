55ac005b530addadb641f4b716c10feb4f55baa8
Andreas Bauer <andreas.bauer@stanford.edu>
2023-11-09 10:41:23 -0800

    Add debounce timing test

---
+++ Tests/SpeziViewsTests/SpeziValidationTests.swift
@@ -0,0 +1,34 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import SpeziValidation
+import XCTest
+
+
+final class SpeziValidationTests: XCTestCase {
+    @MainActor
+    func testValidationDebounce() async throws {
+        let engine = ValidationEngine(rules: .nonEmpty)
+
+        engine.submit(input: "Valid")
+        XCTAssertTrue(engine.inputValid)
+        XCTAssertEqual(engine.validationResults, [])
+
+        engine.submit(input: "", debounce: true)
+        XCTAssertTrue(engine.inputValid)
+        XCTAssertEqual(engine.validationResults, [])
+
+        sleep(1)
+        XCTAssertFalse(engine.inputValid)
+        XCTAssertEqual(engine.validationResults.count, 1)
+
+        engine.submit(input: "Valid", debounce: true)
+        XCTAssertTrue(engine.inputValid) // valid state is reported instantly
+        XCTAssertEqual(engine.validationResults, [])
+    }
+}
