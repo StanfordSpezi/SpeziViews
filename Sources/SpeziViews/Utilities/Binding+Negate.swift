a012d0f04fd47b2078278e15b188a802e3d97717
Philipp Zagar <zagar@stanford.edu>
2024-02-26 13:04:12 -0800

    Add more documentation

--- Sources/SpeziViews/Utilities/Binding+Negate.swift
+++ Sources/SpeziViews/Utilities/Binding+Negate.swift
@@ -10,7 +10,38 @@ import SwiftUI
 
 
 extension Binding where Value == Bool {
-    /// Negates a `Binding`.
+    /// Negates a SwiftUI `Binding`.
+    ///
+    /// ### Usage
+    ///
+    /// The example below demonstrates a minimal use case of a negated SwiftUI `Binding` by toggling a shared boolean state in different `View`s.
+    ///
+    /// ```swift
+    /// struct ParentView: View {
+    ///     @State var isOn: Bool = false
+    ///
+    ///     var body: some View {
+    ///         VStack {
+    ///             Button(isOn ? "Turn Off" : "Turn On") {
+    ///                 isOn.toggle()
+    ///             }
+    ///
+    ///             // Pass an inverted `Binding` to the `ChildView`.
+    ///             ChildView(isOff: !$isOn)
+    ///         }
+    ///    }
+    /// }
+    ///
+    /// struct ChildView: View {
+    ///     @Binding var isOff: Bool
+    ///
+    ///     var body: some View {
+    ///         Button("Toggle from Child View") {
+    ///             isOff.toggle()
+    ///         }
+    ///     }
+    /// }
+    /// ```
     public prefix static func ! (value: Binding<Bool>) -> Binding<Bool> {
         Binding<Bool>(
             get: { !value.wrappedValue },
