fc944730c4934f0c7ba6cd62e33dd24f281e4e81
auddithio nag <58368314+auddithio@users.noreply.github.com>
2024-03-05 13:19:48 -0800

    added documentation to text views

--- Sources/SpeziViews/Views/Text/TextContent.swift
+++ Sources/SpeziViews/Views/Text/TextContent.swift
@@ -8,11 +8,24 @@
 
 import Foundation
 
-
+/// The `TextContent` enum represents either a plain string or a localized string resource.
+///
+/// Use this enum to encapsulate text content in your application, allowing flexibility in handling localized and non-localized strings.
+///
+/// ```swift
+/// // Example usage:
+/// let content: TextContent = .localized(LocalizedStringResource("HELLO_SPEZI", bundle: .main))
+/// let localizedString = content.localizedString(for: .current)
+/// print(localizedString) // Prints the localized string for the current locale.
+/// ```
 enum TextContent {
     case string(_ value: String)
     case localized(_ value: LocalizedStringResource)
 
+    /// Returns the localized string representation of the content for a given locale.
+    ///
+    /// - Parameter locale: The locale for which to retrieve the localized string.
+    /// - Returns: The localized string corresponding to the content.
     func localizedString(for locale: Locale) -> String {
         switch self {
         case let .string(string):
