344b9e19380440dbc7d8203f22af2db0cc1cfa60
Paul Heidekrüger <paul.heidekrueger@tum.de>
2024-08-19 11:31:32 +0200

    docs: add ViewState documentation

--- Sources/SpeziViews/Model/ViewState.swift
+++ Sources/SpeziViews/Model/ViewState.swift
@@ -10,6 +10,52 @@ import Foundation
 
 
 /// Allows SwiftUI views to keep track of their state and communicate to outside views.
+///
+/// A `ViewState` provides a built-in mechanism for tracking the state of a Spezi UI component.
+/// A view can be in either an `idle`, `processing`, or `error` `ViewState`.
+///
+/// ```swift
+/// import SpeziViews
+/// import SwiftUI
+///
+/// enum ProcessingError: LocalizedError {
+///     case resourceError
+/// }
+///
+/// func doSomeProcessing() throws {
+///     // Function may throw an error
+///     throw ProcessingError.resourceError
+/// }
+///
+/// struct OperationStateTestView: View {
+///     @State private var viewState: ViewState = .idle
+///
+///     var body: some View {
+///         VStack {
+///             Button("Action") {
+///                 viewState = .processing
+///                 do {
+///                     try doSomeProcessing()
+///                     viewState = .idle
+///                 } catch let error as LocalizedError {
+///                     viewState = .error(error)
+///                 } catch {
+///                     print("An unknown error occurred: \(error)")
+///                 }
+///             }
+///             .viewStateAlert(state: $viewState)
+///         }
+///     }
+/// }
+/// ```
+///
+/// The ``SwiftUI/View/viewStateAlert(state:)-4wzs4`` view modifier can be used to automatically notify users when the
+/// `ViewState` enters an error state.
+///
+/// > Tip:
+/// > To avoid having to handle state changes manually (as seen above), a `ViewState` can be mapped to an
+/// > `OperationState`, allowing the `ViewState` to change automatically when the application's internal state changes.
+/// > For instruction on how to define such a mapping, see the ``OperationState`` documentation.
 public enum ViewState {
     /// The view is idle and displaying content.
     case idle
