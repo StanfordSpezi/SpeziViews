0345b3c7b3a618ddf0945c4b0640a5a05c32d11b
Lukas Kollmer <hey@lukaskollmer.de>
2025-04-08 15:14:30 +0200

    `ManagedNavigationStack` (#57)
    # `ManagedNavigationStack`
    
    ## :recycle: Current situation & Problem
    SpeziOnboarding currently has, in its `OnboardingStack`, a
    de-facto-generic NavigationStack with the ability to control navigation
    within the stack via external logic.
    Since this is useful for way more than just onboarding, we refactor it
    out of SpeziOnboarding and into SpeziViews.
    The `ManagedNavigationStack`'s API and functionality are essentially the
    same as what the `OnboardingStack` used to have, with some changes:
    - the first step no longer receives an implicit top padding of 24 points
    - we no longer allow `for` loops in the stack's content builder, since
    they break our view identification logic
    - many symbols have been renamed to no longer contain the term
    "onboarding"
    - the Path API (which used to be called `OnboardingNavigationPath` and
    is now called `ManagedNavigationStack.Path`) has been reworked:
    - there is a new `func
    navigateToNextStep(matching:includeIntermediateSteps:)` function, which
    handles forward navigation within the stack
    - the destination step is selected either based on its type, or its
    custom navigation step identifier.
    - the function now also allows specifying what the
    ManagedNavigationStack should do with any intermediate steps (between
    the current step and the destination). the old behaviour was to always
    ignore these and append only the selected step, but there are valid
    applications for including these, especially since the navigation would
    otherwise be more of a skip operation.
    - the `onboardingIdentifier(_:)` view modifier has been replaced with a
    new `navigationStepIdentifier(_:)` modifier. the old version still
    exists as a deprecated forward.
    - the new type is currently called `ManagedNavigationStep`
    - @philippzagar originally suggested calling it `NavigationFlow` (which
    would also be a valid name, IMO, though i'd probably vote for
    `ManagedNavigationStack` instead, since we already have other types
    called `ManagedX` in SpeziViews (eg the `ManagedViewState`), and the
    name "NavigationFlow" feels more like it's referencing the
    configuration/set/order of the individual steps within the stack's
    configuration (in fact, the name `NavigationFlow` is currently used for
    the collection type that stores the actual navigation steps)).
        - still open for feedback and naming suggestions/input, of course
    
    ## :gear: Release Notes
    - Added `ManagedNavigationStack`
    
    ## :books: Documentation
    The code is documented, since it already was documented when it was part
    of SpeziOnboarding. The documentation has been adjusted to reftect the
    move, and also to reflect some changes in behaviour made as part of this
    transition.
    
    ## :white_check_mark: Testing
    There are tests to cover the new code.
    
    ## :pencil: Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

---
+++ Tests/SpeziViewsTests/ManagedNavigationStackStepIdentifierTests.swift
@@ -0,0 +1,27 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import SpeziViews
+import SwiftUI
+import Testing
+
+
+@Suite("ManagedNavigationStackIdentifierTests")
+struct ManagedNavigationStackIdentifierTests {
+    @Test("OnboardingIdentifier ViewModifier")
+    @MainActor
+    func testOnboardingIdentifierModifier() throws {
+        let stack = ManagedNavigationStack {
+            Text("Hello World")
+                .navigationStepIdentifier("Custom Identifier")
+        }
+        
+        let identifier = try #require(stack.path.firstStepIdentifier)
+        #expect(identifier.identifierKind == .identifiable("Custom Identifier"))
+    }
+}
