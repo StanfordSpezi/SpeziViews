67481111f84d1bb590dae5567761d53847e2f0c7
Andreas Bauer <andreas.bauer@stanford.edu>
2025-06-11 22:48:52 +0200

    Remove usages of runOrScheduleOnMainActor

--- Tests/UITests/TestApp/ViewsTests/CanvasTestView.swift
+++ Tests/UITests/TestApp/ViewsTests/CanvasTestView.swift
@@ -20,6 +20,8 @@ struct CanvasTestView: View {
     @State var drawing = PKDrawing()
     @State var receivedSize: CGSize?
 
+    @State private var values: (stream: AsyncStream<CGSize>, continuation: AsyncStream<CGSize>.Continuation) = AsyncStream.makeStream()
+
     var body: some View {
         ZStack {
             VStack {
@@ -48,9 +50,19 @@ struct CanvasTestView: View {
             }
             .navigationBarTitleDisplayMode(.inline)
             .onPreferenceChange(CanvasView.CanvasSizePreferenceKey.self) { size in
-                runOrScheduleOnMainActor {
-                    self.receivedSize = size
+                if Thread.isMainThread {
+                    MainActor.assumeIsolated {
+                        self.receivedSize = size
+                    }
+                } else {
+                    self.values.continuation.yield(size)
                 }
+            }
+            .task {
+                for await value in values {
+                    self.receivedSize = value
+                }
+                values = AsyncStream.makeStream()
             }
     }
 }
