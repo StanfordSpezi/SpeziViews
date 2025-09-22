546143dc89e8073fd0d8929919a7ca75297950d9
Lukas Kollmer <hey@lukaskollmer.de>
2025-09-22 22:15:56 +0200

    fix

--- Sources/SpeziViews/Styles/LiquidGlass.swift
+++ Sources/SpeziViews/Styles/LiquidGlass.swift
@@ -13,54 +13,42 @@ extension View {
     /// Applies a `glass` button style to the view, if available.
     ///
     /// If the glass button style is not available, e.g. because the app is running on a pre-iOS 26 device,
-    /// the modifier will either apply the `fallback` style (if available), or not do anything.
+    /// the modifier will apply the `fallback` style.
     ///
     /// - parameter fallback: The button style to apply if Liquid Glass is not available. Defaults to the [`automatic`](https://developer.apple.com/documentation/swiftui/primitivebuttonstyle/automatic) button style.
     @ViewBuilder
     public func buttonStyleGlass(
-        fallback: (some PrimitiveButtonStyle)? = DefaultButtonStyle.automatic
+        fallback: some PrimitiveButtonStyle = DefaultButtonStyle.automatic
     ) -> some View {
         #if swift(>=6.2) && !os(visionOS)
         if #available(iOS 26, macOS 26, macCatalyst 26, tvOS 26, watchOS 26, *) {
             self.buttonStyle(.glass)
-        } else if let fallback {
-            self.buttonStyle(fallback)
         } else {
-            self
+            self.buttonStyle(fallback)
         }
         #else
-        if let fallback {
-            self.buttonStyle(fallback)
-        } else {
-            self
-        }
+        self.buttonStyle(fallback)
         #endif
     }
     
     /// Applies a `glass` button style to the view, if available.
     ///
     /// If the glass button style is not available, e.g. because the app is running on a pre-iOS 26 device,
-    /// the modifier will either apply the `fallback` style (if available), or not do anything.
+    /// the modifier will apply the `fallback` style.
     ///
     /// - parameter fallback: The button style to apply if Liquid Glass is not available. Defaults to the [`borderedProminent`](https://developer.apple.com/documentation/swiftui/primitivebuttonstyle/borderedprominent) button style.
     @ViewBuilder
     public func buttonStyleGlassProminent(
-        fallback: (some PrimitiveButtonStyle)? = BorderedProminentButtonStyle.borderedProminent
+        fallback: some PrimitiveButtonStyle = BorderedProminentButtonStyle.borderedProminent
     ) -> some View {
         #if swift(>=6.2) && !os(visionOS)
         if #available(iOS 26, macOS 26, macCatalyst 26, tvOS 26, watchOS 26, *) {
             self.buttonStyle(.glassProminent)
-        } else if let fallback {
-            self.buttonStyle(fallback)
         } else {
-            self
+            self.buttonStyle(fallback)
         }
         #else
-        if let fallback {
-            self.buttonStyle(fallback)
-        } else {
-            self
-        }
+        self.buttonStyle(fallback)
         #endif
     }
 }
