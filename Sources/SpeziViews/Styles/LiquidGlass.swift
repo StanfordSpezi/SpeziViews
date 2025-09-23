d05ffbcedf6760d1a9671f629475c46ee6b1fe19
Lukas Kollmer <hey@lukaskollmer.de>
2025-09-23 13:24:35 +0200

    glassButtonStyle modifier (#77)
    # glassButtonStyle modifier
    
    ## :recycle: Current situation & Problem
    see #76 for a detailed explanation
    
    fixes #76
    
    
    ## :gear: Release Notes
    - added `buttonStyleGlass()` and `buttonStyleGlassProminent()` view
    modifiers
    
    
    ## :books: Documentation
    yes
    
    
    ## :white_check_mark: Testing
    yes
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

---
+++ Sources/SpeziViews/Styles/LiquidGlass.swift
@@ -0,0 +1,54 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SwiftUI
+
+
+extension View {
+    /// Applies a `glass` button style to the view, if available.
+    ///
+    /// If the glass button style is not available, e.g. because the app is running on a pre-iOS 26 device,
+    /// the modifier will apply the `fallback` style.
+    ///
+    /// - parameter fallback: The button style to apply if Liquid Glass is not available. Defaults to the [`automatic`](https://developer.apple.com/documentation/swiftui/primitivebuttonstyle/automatic) button style.
+    @ViewBuilder
+    public func buttonStyleGlass(
+        fallback: some PrimitiveButtonStyle = DefaultButtonStyle.automatic
+    ) -> some View {
+        #if swift(>=6.2) && !os(visionOS)
+        if #available(iOS 26, macOS 26, macCatalyst 26, tvOS 26, watchOS 26, *) {
+            self.buttonStyle(.glass)
+        } else {
+            self.buttonStyle(fallback)
+        }
+        #else
+        self.buttonStyle(fallback)
+        #endif
+    }
+    
+    /// Applies a `glass` button style to the view, if available.
+    ///
+    /// If the glass button style is not available, e.g. because the app is running on a pre-iOS 26 device,
+    /// the modifier will apply the `fallback` style.
+    ///
+    /// - parameter fallback: The button style to apply if Liquid Glass is not available. Defaults to the [`borderedProminent`](https://developer.apple.com/documentation/swiftui/primitivebuttonstyle/borderedprominent) button style.
+    @ViewBuilder
+    public func buttonStyleGlassProminent(
+        fallback: some PrimitiveButtonStyle = BorderedProminentButtonStyle.borderedProminent
+    ) -> some View {
+        #if swift(>=6.2) && !os(visionOS)
+        if #available(iOS 26, macOS 26, macCatalyst 26, tvOS 26, watchOS 26, *) {
+            self.buttonStyle(.glassProminent)
+        } else {
+            self.buttonStyle(fallback)
+        }
+        #else
+        self.buttonStyle(fallback)
+        #endif
+    }
+}
