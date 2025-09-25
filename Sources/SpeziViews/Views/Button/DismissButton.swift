e4f08b7200f38eec605ab4eb2fb4a0437e8eb36b
Lukas Kollmer <hey@lukaskollmer.de>
2025-09-25 21:20:01 +0200

    `DismissButton`: restore pre-swift6.2 compatibility (#78)
    # `DismissButton`: restore pre-swift6.2 compatibility
    
    ## :recycle: Current situation & Problem
    #75 updated the `DismissButton` to use new types and overloads from the
    iOS 26 SDK, but did so without updating the Package.swift file's Swift
    version requirement, as a result breaking the package on Xcode 16.x
    
    
    ## :gear: Release Notes
    - `DismissButton`: restore pre-swift6.2 compatibility
    
    
    ## :books: Documentation
    n/a
    
    
    ## :white_check_mark: Testing
    n/a
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

--- Sources/SpeziViews/Views/Button/DismissButton.swift
+++ Sources/SpeziViews/Views/Button/DismissButton.swift
@@ -14,7 +14,8 @@ public struct DismissButton: View {
     @Environment(\.dismiss) private var dismiss
 
     public var body: some View {
-        #if os(visionOS) || os(tvOS) || os(macOS)
+#if swift(>=6.2)
+    #if os(visionOS) || os(tvOS) || os(macOS)
         if #available(visionOS 26, tvOS 26, macOS 26, *) {
             Button(role: .close) {
                 dismiss()
@@ -24,35 +25,49 @@ public struct DismissButton: View {
                 dismiss()
             }
         }
-        #else
+    #else
         if #available(iOS 26, watchOS 26, *) {
             Button(role: .close) {
                 dismiss()
             }
         } else {
-            Button {
-                dismiss()
-            } label: {
-                Image(systemName: "xmark")
-                    .font(.system(size: 10, weight: .bold, design: .rounded))
-                    #if !os(watchOS)
-                    .foregroundStyle(.secondary)
-                    #endif
-                    .background {
-                        Circle()
-                            #if os(iOS)
-                            .fill(Color(uiColor: .secondarySystemBackground))
-                            #elseif os(watchOS)
-                            .fill(Color(uiColor: .darkGray))
-                            #endif
-                            .frame(width: 25, height: 25)
-                    }
-                    .frame(width: 27, height: 27) // make the tap-able button region slightly larger
-            }
-            .accessibilityLabel("Dismiss")
-            .buttonStyle(.plain)
+            fallbackButton
         }
-        #endif
+    #endif
+#else // swift < 6.2
+        fallbackButton
+#endif
+    }
+    
+    
+    @ViewBuilder private var fallbackButton: some View {
+#if os(visionOS) || os(tvOS) || os(macOS)
+        Button("Dismiss", systemImage: "xmark") {
+            dismiss()
+        }
+#else
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: "xmark")
+                .font(.system(size: 10, weight: .bold, design: .rounded))
+                #if !os(watchOS)
+                .foregroundStyle(.secondary)
+                #endif
+                .background {
+                    Circle()
+                        #if os(iOS)
+                        .fill(Color(uiColor: .secondarySystemBackground))
+                        #elseif os(watchOS)
+                        .fill(Color(uiColor: .darkGray))
+                        #endif
+                        .frame(width: 25, height: 25)
+                }
+                .frame(width: 27, height: 27) // make the tap-able button region slightly larger
+        }
+        .accessibilityLabel("Dismiss")
+        .buttonStyle(.plain)
+#endif
     }
 
     public init() {}
